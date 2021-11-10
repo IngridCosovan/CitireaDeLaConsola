385f1707f6330770bc1c03e879d3b3f91494683c
Ingrid Cosovan <ingrid.cosovan@gmail.com>
2021-11-10 22:33:19 +0200

    Initial commit

---
+++ src/Citire.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Citire {
+
+    public static void main(String[] args) {
+        Scanner cititor = new Scanner (System.in);
+   //     System.out.println("Introduceti textul dorit:");
+     //   String textIntrodus = cititor.nextLine();
+       // System.out.println("Am introdus textul" + textIntrodus);
+        System.out.println("Introduceti numarul dorit:");
+        int numarIntrodus = cititor.nextIn();
+        System.out.println("Ai introdus numarul" + numarIntrodus );
+    }
+}
